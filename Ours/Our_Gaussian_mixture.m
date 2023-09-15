27953970605de8e9494085c699be200f0c6a5b80
yeoneung <102267531+yeoneung@users.noreply.github.com>
2023-09-15 14:24:29 +0900

    Update Our_Gaussian_mixture.m

--- Ours/Our_Gaussian_mixture.m
+++ Ours/Our_Gaussian_mixture.m
@@ -72,5 +72,5 @@ xlabel('Horizon','Fontsize',16)
 ylabel('$|\theta-\theta_*|$','Interpreter','latex')
 save_time = [datestr(now,'mmmm dd, yyyy HH:MM:SS.FFF AM')];
 FileName = "Our_Gaussian_mixture-"+string(n)+"D_"+save_time+".csv";
-writematrix(error,FileName);
+writematrix(error,'FileName');
 end
